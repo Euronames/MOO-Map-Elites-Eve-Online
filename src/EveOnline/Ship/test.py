1ded3b938128565c45d066c41ada686ece454936
Mads Norby <mads.sfg@gmail.com>
2020-05-05 13:29:58 +0200

    yo

--- src/EveOnline/Ship/test.py
+++ src/EveOnline/Ship/test.py
@@ -10,11 +10,6 @@ from jnius import autoclass
 
 Ship = autoclass('EveOnline.Ship.Ship')
 
-
-
-
-
-
 @given(\
     numberOfRigs = st.integers(max_value=10000, min_value=0)\
     , numberOfHighModules = st.integers(max_value=10000, min_value=0)\
@@ -22,11 +17,28 @@ Ship = autoclass('EveOnline.Ship.Ship')
     , numberOfLowModules = st.integers(max_value=10000, min_value=0)\
     , cpuTotal = st.floats(max_value=10000, min_value=0)\
     , powerTotal = st.floats(max_value=10000, min_value=0)\
-    , calibrationTotal = st.floats(max_value=10000, min_value=0))
-def generate_ship(numberOfRigs, numberOfHighModules, numberOfMediumModules, numberOfLowModules, cpuTotal, powerTotal, calibrationTotal):
-    new_ship = Ship(numberOfRigs, numberOfHighModules, numberOfMediumModules, numberOfLowModules, cpuTotal, powerTotal, calibrationTotal)
-    print(new_ship.toString())
-    assert(new_ship != None)
+    , calibrationTotal = st.floats(max_value=10000, min_value=0)\
+    , ship_name = st.text(min_size=10, max_size=100))
+def generate_ship(\
+    numberOfRigs\
+    , numberOfHighModules\
+    , numberOfMediumModules\
+    , numberOfLowModules\
+    , cpuTotal\
+    , powerTotal\
+    , calibrationTotal\
+    , ship_name):
+    new_ship = Ship(\
+        numberOfRigs\
+        , numberOfHighModules\
+        , numberOfMediumModules\
+        , numberOfLowModules\
+        , cpuTotal\
+        , powerTotal\
+        , calibrationTotal)
+    new_ship.addTypeName(ship_name)
+    #TODO: test_feature_space(ship)
+ 
 
-
-generate_ship()
+ def test_feature_space(ship):
+     print("TESTING FEATURESPACE")
